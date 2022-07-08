230c606ef0b8a77c8ce7a373b5944cea854e89e5
andrea sgobbi <andrea.g.sgobbi@gmail.com>
2022-07-08 14:31:10 +0200

    linux fix

--- main.c
+++ main.c
@@ -2,22 +2,12 @@
 #include "game.h"
 
 
-double insert_time = 0.0;
-double insert_ord_time = 0.0;
-double search_time = 0.0;
-double fill_time = 0.0;
-double filter_time = 0.0;
-double total_time = 0.0;
-double print_time = 0.0;
-
 int main(){
     dict_ptr word_dict = generate_dict();
     char *buff;
     int wordsize, restart;
-    FILE *input = fopen("/Users/andreasgobbi/Documents/uni_works/progetto_api/dum_e/gen_large.txt", "r");
-    FILE *output = fopen("/Users/andreasgobbi/Documents/uni_works/progetto_api/dum_e/dump.txt", "w");
-    int i;
-    clock_t start = clock();
+    FILE *input = fopen("/home/andrea/Documents/progetto_api/dum_e/test3.txt", "r");
+    FILE *output = fopen("/home/andrea/Documents/progetto_api/dum_e/dump.txt", "w");
 
     // save initial wordlist on the tree
     safe_scanf(&wordsize, input);
@@ -34,20 +24,10 @@ int main(){
     if (wordsize <= 14) while (fgetc(input) != '\n'); // dumps leftover
     free(buff);
 
-    i = 1;
     do { // main game loop
         restart = new_game(word_dict, wordsize, input, output);
-        printf("\nGAME %d FINISHED", i);
-        ++i;
     } while(restart == 1);
 
     free_dict(word_dict);
-
-    clock_t stop = clock();
-    total_time = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
-
-    printf("\n\nTOTAL TIME:%lf\nINSERT: %lf\nORDERED INS: %lf\nSEARCH: %lf\nFILL: %lf\nFILTER: %lf\nPRINT: %lf\n",
-    total_time, insert_time, insert_ord_time, search_time, fill_time, filter_time, print_time);
-    //scanf("c");
     exit(EXIT_SUCCESS);
 }
